6a1295f8b11ac5f412cebeebf0af13f78714bf58
Emily Lewis <emilymlewis0@gmail.com>
2024-09-10 18:53:07 -0400

    Add login page with Google auth

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,12 +1,24 @@
+import { useSupabaseClient } from "./hooks";
 import logo from "/KeyTrackLogo.svg";
 
 export const Auth: React.FC = () => {
+  const supabase = useSupabaseClient();
+
+  const login = async () => {
+    // TODO handle sign in and sign out errors
+    // const { error } = await supabase.auth.signInWithOAuth({
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
   return (
     <>
       <div>
         <img src={logo} alt="KeyTrack logo" />
       </div>
       <h1>Login page</h1>
+      <button onClick={login}>Login</button>
     </>
   );
 };

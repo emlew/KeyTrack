231569c1abc8a192ed23f44cf5a12043a84739d8
Emily Lewis <emilymlewis0@gmail.com>
2024-09-17 14:00:25 -0400

    Add admin user creation

---
+++ src/pages/Home/Home.styles.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const StyledPage = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+  alignItems: "flex-start",
+});

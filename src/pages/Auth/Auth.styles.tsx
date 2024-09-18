8e5e979ba647c67e2dd370b2b5d6b0f1783e05f9
Emily Lewis <emilymlewis0@gmail.com>
2024-09-17 20:26:27 -0400

    Extract login components

--- src/pages/Auth/Auth.styles.tsx
+++ src/pages/Auth/Auth.styles.tsx
@@ -3,13 +3,13 @@ import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 
 export const StyledPage = styled(Box)({
-    display: "flex",
-    height: "100vh",
-    width: "100vw",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: theme.palette.primary[400],
-})
+  display: "flex",
+  height: "100vh",
+  width: "100vw",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: theme.palette.primary[400],
+});
 
 export const ContentWrapper = styled(Box)({
   display: "flex",
@@ -19,19 +19,14 @@ export const ContentWrapper = styled(Box)({
   backgroundColor: theme.palette.common.white,
   padding: "40px",
   alignItems: "center",
+  justifyContent: "center",
   borderRadius: "10px",
-  height: "40vh",
+  height: "50vh",
+  minWidth: "500px",
 });
 
-export const StyledLoginForm = styled(Box)({
-    display: "flex",
-    flexDirection: "column",
-    gap: "20px",
-    alignItems: "flex-start",
-})
-
 export const StyledDivider = styled(Box)({
-    width: "1px",
-    height: "30vh",
-    backgroundColor: theme.palette.primary[800],
-})
+  width: "1px",
+  height: "40vh",
+  backgroundColor: theme.palette.primary[800],
+});

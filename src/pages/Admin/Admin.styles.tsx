3a14d3abf7021a81158d3bee3783c16ca5baa89d
Emily Lewis <emilymlewis0@gmail.com>
2024-09-23 17:14:10 -0400

    Account and Admin UI

---
+++ src/pages/Admin/Admin.styles.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const StyledPage = styled(Box)({
+  display: "inline-flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "40px",
+  width: "100%",
+});
+export const StyledContent = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
+  width: "100%",
+  gap: "40px",
+  alignContent: "flex-start",
+});
--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,15 +1,26 @@
-import { useSupabase } from "@/hooks";
-import { TextField, Button } from "@mui/material";
+import { Card, Table, Title } from "@/components";
+import { useAdminSupabase, useAllUsersData } from "@/hooks";
+import {
+  TextField,
+  Button,
+  Typography,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@mui/material";
 import { useState } from "react";
+import { StyledContent, StyledPage } from "./Admin.styles";
 
 export const Admin: React.FC = () => {
-  const supabase = useSupabase();
   const [invitee, setInvitee] = useState("");
   const [hasError, setHasError] = useState(false);
   const [helperText, setHelperText] = useState("");
+  const createUser = useAdminSupabase();
+  const { data: data } = useAllUsersData();
 
   const create = async () => {
-    const { error } = await supabase.auth.signUp({
+    // TODO: add bulk account creation option
+    const { error } = await createUser.createUser({
       email: invitee,
       password: "KeyClub24",
     });
@@ -17,32 +28,48 @@ export const Admin: React.FC = () => {
       setHasError(true);
       setHelperText(error.message);
     } else {
-      setHelperText("Success!");
+      setHelperText(`User ${invitee} successfully created!`);
+      setHasError(false);
+      // TODO: add snackbars for feedback
     }
   };
 
-  const resetCreate = () => {
-    setHasError(false);
-    setHelperText("");
-  };
   return (
-    <>
-      <TextField
-        variant="outlined"
-        label="User"
-        value={invitee}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          setInvitee(event.target.value);
-        }}
-        error={hasError}
-        helperText={helperText}
-      />
-      <Button
-        variant={"contained"}
-        onClick={helperText === "Success!" ? resetCreate : create}
-      >
-        {helperText === "Success!" ? "Create another user" : "Create"}
-      </Button>
-    </>
+    <StyledPage>
+      <Title title="Administrator View" />
+      <StyledContent>
+        <Card>
+          <Typography variant="h6">Create a User</Typography>
+          <TextField
+            variant="outlined"
+            label="User Email"
+            value={invitee}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setInvitee(event.target.value);
+            }}
+            error={hasError}
+            helperText={helperText}
+          />
+          <Button variant={"contained"} onClick={create}>
+            Create
+          </Button>
+        </Card>
+        <Table columnNames={["Email", "Role"]}>
+          <TableBody>
+            {data &&
+              data.users.map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell component="th" scope="row">
+                    {user.email}
+                  </TableCell>
+                  <TableCell align="right">
+                    {user.role === "kt_admin" ? "Admin" : "Member"}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </StyledContent>
+    </StyledPage>
   );
 };

464b77392d98f9ab67518bf76f072613f71b9741
Emily Lewis <emilymlewis0@gmail.com>
2024-10-10 14:02:44 -0400

    Remove auth header

--- src/hooks/useSupabase.tsx
+++ src/hooks/useSupabase.tsx
@@ -12,11 +12,6 @@ const getSupabaseBrowserClient = () => {
   client = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_ANON_KEY,
-    {
-      global: {
-        headers: { Authorization: import.meta.env.VITE_SUPABASE_ANON_KEY },
-      },
-    }
   );
 
   return client;

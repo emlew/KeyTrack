a42f808ba4aa990f8a19cfccb739f92fc76fcd80
Emily Lewis <emilymlewis0@gmail.com>
2024-11-29 12:33:31 -0600

    Generate user report hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,7 @@ export * from "./useCreateEvent";
 export * from "./useCreateHours";
 export * from "./useDrawer";
 export * from "./useEventsData";
+export * from "./useGenerateReport";
 export * from "./useHoursData";
 export * from "./useInvalidateQueries";
 export * from "./useModal";
